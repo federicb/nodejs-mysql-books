9dd1db9b8634dcbb222ff9df7a3112246f2508a1
Federico <isma-fb@hotmail.com>
2023-02-06 17:29:53 +0100

    conexion a db books_express correcta

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -6,11 +6,11 @@ console.log(USER)
 console.log(PASSWORD)
 console.log(DATABASE)
 
-// module.exports = () => {
-//     return mysql.createConnection({
-//         host: HOST,
-//         user: USER,
-//         password: PASSWORD,
-//         database: DATABASE
-//     })
-// }
+module.exports = () => {
+    return mysql.createConnection({
+        host: HOST,
+        user: USER,
+        password: PASSWORD,
+        database: DATABASE
+    })
+}
